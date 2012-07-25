666ae8639a0737592b0678e6faafebe18ad00f96
sgelb <stephan.halbritter@haw-hamburg.de>
2012-07-25 23:58:25 +0200

    can add/index/destroy products. added validation

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,34 @@
 class ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(params[:product])
+
+    if @product.save
+      flash[:notice] = 'Saved new product!'
+      redirect_to @product
+    else
+      render action: 'new'
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_url
+  end
+
+  def edit
+  end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,12 @@
 class Product < ActiveRecord::Base
   attr_accessible :price, :title
+
+  # we don't want empty fields
+  validates :title, :price, presence: true
+
+  # price must have special format and be greater than 0
+  validates :price, format: { :with => /^\d+??(?:\.\d{0,2})?$/ }, :numericality => {greater_than: 0}
+
+  # every product name must be unique
+  validates :title, uniqueness: true
 end
