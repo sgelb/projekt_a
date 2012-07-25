65554d8957b0a5cb0db8321c4182d1eb17c4e3f2
sgelb <stephan.halbritter@haw-hamburg.de>
2012-07-25 22:55:27 +0200

    created Product model/controller/views/routes

---
+++ db/migrate/20120725204752_create_products.rb
@@ -0,0 +1,10 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.decimal :price, :precision => 7, :scale => 2
+
+      t.timestamps
+    end
+  end
+end
