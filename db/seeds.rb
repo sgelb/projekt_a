10cf92e2a51af8c9b46f742d1c61407a1a61a8df
sgelb <stephan.halbritter@haw-hamburg.de>
2012-08-06 15:40:52 +0200

    Edited seeds.rb, added habtm-relationship products<->ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,24 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Product.create({title:"Margeritha", price:"1.23"})
-Product.create({title:"Funghi", price:"3.23"})
-Product.create({title:"Montanero", price:"5.54"})
-Product.create({title:"Spinat", price:"9.99"})
+i1 = Ingredient.create({name:"Tomatoes", quantity:"6", price:"0.81"})
+i2 = Ingredient.create({name:"Spinach", quantity:"4", price:"2.11"})
+i3 = Ingredient.create({name:"Fake Cheese", quantity:"12", price:"3.11"})
+i4 = Ingredient.create({name:"Mushrooms", quantity:"5", price:"1.11"})
 
-Ingredient.create({name:"Mushrooms", quantity:"5", price:"1.11"})
-Ingredient.create({name:"Spinach", quantity:"4", price:"2.11"})
-Ingredient.create({name:"Fake Cheese", quantity:"12", price:"3.11"})
+p1 = Product.new({title:"Margeritha", price:"1.23"})
+p2 = Product.new({title:"Funghi", price:"3.23"})
+p3 = Product.new({title:"Montanero", price:"5.54"})
+p4 = Product.new({title:"Spinach", price:"9.99"})
+
+p1.ingredients << i1
+p1.ingredients << i3
+p2.ingredients << i4
+p3.ingredients << i3
+p4.ingredients << i2
+p4.ingredients << i3
+
+p1.save
+p2.save
+p3.save
+p4.save
